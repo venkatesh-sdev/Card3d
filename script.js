9a3bc52e5df974e4454192952a137bbc5d6b5b32
Venkatesh S <venkatesh.dev.main@gmail.com>
2023-02-15 18:30:53 +0530

    pointer add -1

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 window.onload = () => {
-    if (innerWidth < 700) {
-        document.addEventListener("touchmove", (event) => {
-            const div = document.querySelector("#div");
-            const touchLocation = event.targetTouches[0];
-            const x = touchLocation.clientX;
-            const y = touchLocation.clientY;
-            rotateElement(div, x, y);
-        });
-    } else {
-        document.addEventListener("mousemove", (event) => {
-            const div = document.querySelector("#div");
-            const x = event.clientX;
-            const y = event.clientY;
-            rotateElement(div, x, y);
-        });
-    }
-    var pointer = document.querySelector('.pointer');
+    if(innerWidth>700){
+    document.addEventListener("mousemove", (event) => {
+        const div = document.querySelector("#div");
+        const x = event.clientX;
+        const y = event.clientY;
+        rotateElement(div, x, y);
+    });}
+
+    const pointer = document.querySelector('.pointer');
 
     pointer.addEventListener('touchmove', function (e) {
         const touchLocation = e.targetTouches[0];
         pointer.style.left = touchLocation.pageX + 'px';
         pointer.style.top = touchLocation.pageY + 'px';
+        const middleX = window.innerWidth / 2;
+        const middleY = window.innerHeight / 2;
+
+        const offsetX = ((touchLocation.pageX - middleX) / middleX) * 45;
+        const offsetY = ((touchLocation.pageY - middleY) / middleY) * 45;
+
+        div.style.setProperty("--rotateX", offsetX + "deg");
+        div.style.setProperty("--rotateY", -1 * offsetY + "deg");
     })
 
     pointer.addEventListener('touchend', function (e) {
